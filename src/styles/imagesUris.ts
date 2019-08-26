7e89980b2d3e025f7182b0db2a5ddbb9842bd707
Sebastian Perez <sperezm97@gmail.com>
2019-08-26 18:48:45 -0400

    add Images

--- src/styles/imagesUris.ts
+++ src/styles/imagesUris.ts
@@ -2,19 +2,28 @@
 //           IMAGES | ICONS URL MANAGEMENT          //
 // ///////////////////////////////////////////////////
 
-export const icons = {
-
-}
+export const icons = {};
 
 export const images = {
-
-}
+  logo: {
+    uri: require('../assets/images/sounditto-logo.png'),
+  },
+  logo_white: {
+    uri: require('../assets/images/sounditto-logo-blanco.png'),
+  },
+  logo_icon: {
+    uri: require('../assets/images/sounditto-icono.png'),
+  },
+  logo_icon_white: {
+    uri: require('../assets/images/sounditto-icono-blanco.png'),
+  },
+};
 
 export const bottomBar = {
   listening: {
-    uri: require('../assets/images/bottom-bar/30.png') 
+    uri: require('../assets/images/bottom-bar/30.png'),
   },
-  history : {
-    uri: require('../assets/images/bottom-bar/30.png') 
-  }
-}
+  history: {
+    uri: require('../assets/images/bottom-bar/30.png'),
+  },
+};
